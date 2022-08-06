5e5a460f0d58a8676e3856e4d67206f61b3d998b
Natanael Vieira <natanaelvieira130@gmail.com>
2022-08-06 18:50:45 -0300

    M2S10 - Exercicio 09

---
+++ M2S10/M2S10/Controllers/PlaylistController.cs
@@ -0,0 +1,75 @@
+﻿using M2S10.DTOs;
+using M2S10.Models;
+using M2S10.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace M2S10.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase {
+
+        private readonly MusicaRepository _musicaRepository;
+        private readonly PlaylistRepository _playlistRepository;
+
+        public PlaylistController(PlaylistRepository playlistRepository,MusicaRepository musicaRepository,ArtistaRepository artistaRepository) {
+            _musicaRepository = musicaRepository;
+            _playlistRepository = playlistRepository;
+        }
+
+        [HttpGet]
+        public List<Playlist> Get() {
+            return _playlistRepository.RetornaTodasAsPlaylists();
+        }
+
+        [HttpGet("{nome}")]
+        public List<Playlist> Get(string nome) {
+            return _playlistRepository.ObterListaPorNome(nome);
+        }
+
+        [HttpPost]
+        public Playlist Post([FromBody] PlaylistDTO playlistDTO) {
+            if(playlistDTO.IdsMusicas.Count > 0) {
+                List<Musica> musicas = retornaListaDeMusicas(playlistDTO.IdsMusicas);
+                if(musicas.Count > 0) {
+                    Playlist novoPlaylist = new();
+                    novoPlaylist.Nome = playlistDTO.Nome;
+                    novoPlaylist.Musicas = musicas;
+                    _playlistRepository.IncluiPlaylist(novoPlaylist);
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public Playlist Put(int id,[FromBody] PlaylistDTO playlistDTO) {
+            if(playlistDTO.IdsMusicas.Count > 0) {
+                List<Musica> musicas = retornaListaDeMusicas(playlistDTO.IdsMusicas);
+                if(musicas.Count > 0) {
+                    Playlist playlist = new Playlist();
+                    playlist.Nome = playlistDTO.Nome;
+                    playlist.Musicas = musicas;
+                    return _playlistRepository.AlteraPlaylist(id,playlist);
+                }
+            }
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+            _playlistRepository.EliminaPlaylist(id);
+        }
+
+        private List<Musica> retornaListaDeMusicas(List<int> IdMusicas) {
+            List<Musica> musicas = new();
+            for(int i = 0;i < IdMusicas.Count;i++) {
+                Musica musica = _musicaRepository.ObtermusicaPorId(IdMusicas[i]);
+                if(musica != null)
+                    musicas.Add(musica);
+            }
+            return musicas;
+        }
+    }
+}
---
+++ M2S10/M2S10/Repositories/PlaylistRepository.cs
@@ -0,0 +1,47 @@
+﻿using M2S10.DTOs;
+using M2S10.Models;
+
+namespace M2S10.Repositories {
+    public class PlaylistRepository {
+
+        private static int _IdPlaylist;
+        private static List<Playlist> _playlists = new();
+
+        public Playlist ObterPlaylistPorId(int id) {
+            Playlist playlist = _playlists.FirstOrDefault(playlist => playlist.Id == id);
+            return playlist;
+        }
+
+        public List<Playlist> RetornaTodasAsPlaylists() {
+            return _playlists;
+        }
+
+        public List<Playlist> ObterListaPorNome(string nome) {
+            List<Playlist> playlists = _playlists.FindAll(playlist => playlist.Nome == nome);
+            return playlists;
+        }
+
+        public Playlist IncluiPlaylist(Playlist playlist) {
+            Playlist novoPlaylist = new Playlist();
+            novoPlaylist.Nome = playlist.Nome;
+            novoPlaylist.Id = _IdPlaylist;
+            _IdPlaylist++;
+            _playlists.Add(novoPlaylist);
+            return novoPlaylist;
+        }
+
+        public Playlist AlteraPlaylist(int id,Playlist playlist) {
+            Playlist? playlistAlterado = _playlists.FirstOrDefault(playlist => playlist.Id == id);
+            if(playlistAlterado != null) {
+                playlistAlterado.Nome = playlist.Nome;
+                playlistAlterado.Musicas = playlist.Musicas;
+            }
+            return playlistAlterado;
+        }
+
+        public void EliminaPlaylist(int id) {
+            Playlist playlist = _playlists.FirstOrDefault(playlist => playlist.Id == id);
+            _playlists.Remove(playlist);
+        }
+    }
+}
