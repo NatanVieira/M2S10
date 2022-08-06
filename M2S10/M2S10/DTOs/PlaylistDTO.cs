95b17795946a57cedf951de118dfdbf79b60bbdd
Natanael Vieira <natanaelvieira130@gmail.com>
2022-08-06 18:44:29 -0300

    M2S10 - Exercicio 08

---
+++ M2S10/M2S10/DTOs/PlaylistDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace M2S10.DTOs {
+    public class PlaylistDTO {
+
+        public string Nome { get; set; }
+
+        public List<int> IdsMusicas { get; set; }
+    }
+}
---
+++ M2S10/M2S10/Models/Playlist.cs
@@ -0,0 +1,10 @@
+﻿namespace M2S10.Models {
+    public class Playlist {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public List<Musica> Musicas { get; set; }
+    }
+}
