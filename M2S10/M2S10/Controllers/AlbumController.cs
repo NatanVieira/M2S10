314519c4b205966d5197a3f373cb1fdf43c88d4d
Natanael Vieira <natanaelvieira130@gmail.com>
2022-08-06 18:38:15 -0300

    M2S10 - Exercicio 06

---
+++ M2S10/M2S10/Controllers/AlbumController.cs
@@ -0,0 +1,79 @@
+﻿using M2S10.DTOs;
+using M2S10.Models;
+using M2S10.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace M2S10.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase {
+
+        private readonly MusicaRepository _musicaRepository;
+        private readonly AlbumRepository _albumRepository;
+        private readonly ArtistaRepository _artistRepository;
+
+        public AlbumController(AlbumRepository albumRepository,MusicaRepository musicaRepository,ArtistaRepository artistaRepository) {
+            _musicaRepository = musicaRepository;
+            _albumRepository = albumRepository;
+            _artistRepository = artistaRepository;
+        }
+
+        [HttpGet]
+        public List<Album> Get() {
+            return _albumRepository.RetornaTodosOsAlbums();
+        }
+
+        [HttpGet("{nome}")]
+        public List<Album> Get(string nome) {
+            return _albumRepository.ObterListaPorNome(nome);
+        }
+
+        [HttpPost]
+        public Album Post([FromBody] AlbumDTO albumDTO) {
+            Artista artista = _artistRepository.ObterArtistaPorId(albumDTO.IdArtista);
+            if(artista != null && albumDTO.IdsMusicas.Count > 0) {
+                List<Musica> musicas = retornaListaDeMusicas(albumDTO.IdsMusicas);
+                if(musicas.Count > 0) {
+                    Album novoAlbum = new();
+                    novoAlbum.Nome = albumDTO.Nome;
+                    novoAlbum.Compositor = artista;
+                    novoAlbum.Musicas = musicas;
+                    _albumRepository.IncluiAlbum(novoAlbum);
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public Album Put(int id,[FromBody] AlbumDTO albumDTO) {
+            if(albumDTO.IdsMusicas.Count > 0) {
+                List<Musica> musicas = retornaListaDeMusicas(albumDTO.IdsMusicas);
+                if(musicas.Count > 0) {
+                    Album album = new Album();
+                    album.Nome = albumDTO.Nome;
+                    album.Musicas = musicas;
+                    return _albumRepository.AlteraAlbum(id, album);
+                }
+            }
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+            _albumRepository.EliminaAlbum(id);
+        }
+
+        private List<Musica> retornaListaDeMusicas(List<int> IdMusicas) {
+            List<Musica> musicas = new();
+            for(int i = 0;i < IdMusicas.Count;i++) {
+                Musica musica = _musicaRepository.ObtermusicaPorId(IdMusicas[i]);
+                if(musica != null)
+                    musicas.Add(musica);
+            }
+            return musicas;
+        }
+    }
+}
--- M2S10/M2S10/Repositories/AlbumRepository.cs
+++ M2S10/M2S10/Repositories/AlbumRepository.cs
@@ -1,13 +1,48 @@
-﻿using M2S10.Models;
+﻿using M2S10.DTOs;
+using M2S10.Models;
 
 namespace M2S10.Repositories {
     public class AlbumRepository {
 
-        private static int _IdAlbum = 1;
-        private static List<Album> _albuns = new();
+        private static int _IdAlbum;
+        private static List<Album> _albums = new();
 
-        public Album OberPorId(int id) {
-            return _albuns.FirstOrDefault(album => album.Id == id);
+        public Album ObterAlbumPorId(int id) {
+            Album album = _albums.FirstOrDefault(album => album.Id == id);
+            return album;
+        }
+
+        public List<Album> RetornaTodosOsAlbums() {
+            return _albums;
+        }
+
+        public List<Album> ObterListaPorNome(string nome) {
+            List<Album> albums = _albums.FindAll(album => album.Nome == nome);
+            return albums;
+        }
+
+        public Album IncluiAlbum(Album album) {
+            Album novoAlbum = new Album();
+            novoAlbum.Nome = album.Nome;
+            novoAlbum.Compositor = album.Compositor;
+            novoAlbum.Id = _IdAlbum;
+            _IdAlbum++;
+            _albums.Add(novoAlbum);
+            return novoAlbum;
+        }
+
+        public Album AlteraAlbum(int id,Album album) {
+            Album? albumAlterado = _albums.FirstOrDefault(album => album.Id == id);
+            if(albumAlterado != null) {
+                albumAlterado.Nome = album.Nome;
+                albumAlterado.Musicas = album.Musicas;
+            }
+            return albumAlterado;
+        }
+
+        public void EliminaAlbum(int id) {
+            Album album = _albums.FirstOrDefault(album => album.Id == id);
+            _albums.Remove(album);
         }
     }
 }
